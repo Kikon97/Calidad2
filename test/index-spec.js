f10793e151bb5274cd3eb8385d1c48abe91b8985
Unknown <kikonmontoya97@gmail.com>
2018-11-15 18:32:39 -0600

    hola
    he

---
+++ test/index-spec.js
@@ -0,0 +1,10 @@
+var assert = requiere('assert');
+var calculator = require('../index');
+describe('calculator',function (){
+	describe('add function',function(){
+		it('adds numbers',function(){
+			var result = calculator.add(1,2);
+			asset.equal(result,2);
+		});
+	});
+});
